94c3304c4f9302e2cfe530b1e328fc2e68d77d84
maiconVegas <maiconaruquipa@gmail.com>
2025-02-04 14:38:27 -0300

    wip

--- EggTimer/EggTimer.Modelos/Modelos/TimerTask.cs
+++ EggTimer/EggTimer.Modelos/Modelos/TimerTask.cs
@@ -1,11 +1,18 @@
-﻿namespace EggTimer.Modelos;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EggTimer.Modelos;
 public class TimerTask
 {
+    [Required]
     public int Id { get; set; }
+    [Required(ErrorMessage = "Nome da Tarefa é obrigatória")]
+    [MaxLength(100, ErrorMessage = "O tamanho do Nome da Tarefa não pode exceder 100 caracteres")]
+    public string NomeTarefa { get; set; }
     public DateOnly Data { get; set; }
     public TimeSpan HorarioInicio { get; set; }
     public TimeSpan HorarioFim { get; set; }
-    public string NomeTarefa { get; set; }
+    [Required(ErrorMessage = "Tempo Cronometrado é obrigatório")]
     public TimeSpan TempoCronometrado { get; set; }
+    [Required(ErrorMessage = "Status é requerido")]
     public string Status { get; set; }
 }
--- EggTimer/EggTimer/Controllers/TimerTaskController.cs
+++ EggTimer/EggTimer/Controllers/TimerTaskController.cs
@@ -1,4 +1,5 @@
-﻿using EggTimer.API.Responses;
+﻿using System.Reflection.Metadata.Ecma335;
+using EggTimer.API.Responses;
 using EggTimer.Modelos;
 using Microsoft.AspNetCore.Mvc;
 namespace EggTimer.API.Controllers;
@@ -8,7 +9,7 @@ namespace EggTimer.API.Controllers;
 public class TimerTaskController : ControllerBase
 {
     private static List<TimerTask> _tasks = new List<TimerTask>();
-    private static ICollection<TimerTaskResponse> EntityListToResponseList(IEnumerable<TimerTask> listaDeTasks)
+    private static IEnumerable<TimerTaskResponse> EntityListToResponseList(IEnumerable<TimerTask> listaDeTasks)
     {
         return listaDeTasks.Select(a => EntityToResponse(a)).ToList();
     }
@@ -16,6 +17,7 @@ public class TimerTaskController : ControllerBase
     {
         return new TimerTaskResponse(task.NomeTarefa, task.Status);
     }
+
     [HttpPost]
     public void AdicionarTimerTask([FromBody]TimerTask task)
     {
@@ -28,4 +30,27 @@ public class TimerTaskController : ControllerBase
     {
         return Ok(EntityListToResponseList(_tasks));
     }
+
+    [HttpGet("id/{id}")]
+    public IActionResult RecuperarTimerTaskPorId(int id)
+    {
+        var task = _tasks.FirstOrDefault(task => task.Id == id);
+        if (task is null)
+        {
+            return NotFound();
+        }
+        return Ok(EntityToResponse(task));
+    }
+
+    [HttpGet("nome/{nome}")]
+    public IActionResult RecuperarTimerTaskPorNome(string nome)
+    {
+        var task = _tasks.FirstOrDefault(t => t.NomeTarefa.ToUpper().Equals(nome.ToUpper()));
+        if (task is null)
+        {
+            return NotFound();
+        }
+        return Ok(EntityToResponse(task));
+    }
+
 }
