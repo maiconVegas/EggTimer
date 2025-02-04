69b7324a442b70a4978c501728a34692eae8fafc
maiconVegas <maiconaruquipa@gmail.com>
2025-02-04 11:58:31 -0300

    wip

---
+++ EggTimer/EggTimer/Controllers/TimerTaskController.cs
@@ -0,0 +1,23 @@
+﻿using EggTimer.Modelos;
+using Microsoft.AspNetCore.Mvc;
+namespace EggTimer.API.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class TimerTaskController : ControllerBase
+{
+    private static List<TimerTask> _tasks = new List<TimerTask>();
+
+    [HttpPost]
+    public void AdicionarTimerTask([FromBody]TimerTask task)
+    {
+        _tasks.Add(task);
+        Console.WriteLine(task.Data);
+    }
+
+    [HttpGet]
+    public List<TimerTask> ListarTimerTask()
+    {
+        return _tasks;
+    }
+}
