5ff1d95719535ffb1dd13131032a310e67ce1101
maiconVegas <maiconaruquipa@gmail.com>
2025-02-04 11:38:11 -0300

    adicionando modelo e separando a responsabilidade dele do projeto

---
+++ EggTimer/EggTimer.Modelos/Modelos/TimerTask.cs
@@ -0,0 +1,11 @@
+﻿namespace EggTimer.Modelos;
+public class TimerTask
+{
+    public int Id { get; set; }
+    public DateTime Data { get; set; }
+    public TimeSpan HorarioInicio { get; set; }
+    public TimeSpan HorarioFim { get; set; }
+    public string NomeTarefa { get; set; }
+    public TimeSpan TempoCronometrado { get; set; }
+    public string Status { get; set; }
+}
