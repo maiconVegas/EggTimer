e2900ff0851ff9f13e35d4d14569c49cbdc0dbc8
maiconVegas <maiconaruquipa@gmail.com>
2025-02-06 15:58:13 -0300

    adicionando o CORS e usar ele para permitir que os endpoints do API seja utilizado em diversos diretorios e projetos

--- EggTimer/EggTimer/Program.cs
+++ EggTimer/EggTimer/Program.cs
@@ -9,6 +9,19 @@ builder.Services.AddTransient<DAL<TimerTask>>();
 
 //builder.Services.AddSingleton<TimerTaskService>();
 
+// Adicionando o baguiu do CORS para que eu possa utilizar pelo ajax do javascript, não sabia disso e to aprendendo
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        policy =>
+        {
+            policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+        });
+});
+
+
 // Add services to the container.
 
 builder.Services.AddControllers();
@@ -17,6 +30,9 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+
+//aqui em baixo é usar o CORS antes dos controladores, usar de acordo que foi construido no build
+app.UseCors("AllowAll");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
