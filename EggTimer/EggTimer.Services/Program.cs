1394f63c47be2d5ac3772e3bf42ef1ab37832bdd
maiconVegas <maiconaruquipa@gmail.com>
2025-02-11 18:16:05 -0300

    adicionando comentarios para fazer depois sobre refatoração

--- EggTimer/EggTimer.Services/Program.cs
+++ EggTimer/EggTimer.Services/Program.cs
@@ -4,6 +4,9 @@ using EggTimer.Services.Services;
 
 using (var contexto = new EggTimerContext())
 {
+    //tenta refatorar de acordo com o metodo exercutar do timertaskservice, onde que deixar o context
+    // no metodo e aqui so executar o metodo da classe sem usar o context, pois esta sendo usado pelo metodo
+    // O intuito disso é para nao repetir essa declaração e tambem para deixar codigo mais limpo
     var service = new TimerTaskService(contexto);
     service.Executar();
 }
